11be90ef10e4567ae0453abc1d06129f86b48e59
darkoror <roman.k@coaxsoft.com>
2021-05-25 13:40:36 +0300

    get back AuthorPostView and PostView

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,16 +1,11 @@
 from rest_framework import serializers
 
-from blog import constants
 from blog.models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
+class AuthorPostSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
     author = serializers.CharField(default=serializers.CurrentUserDefault())
-
-    default_error_messages = {
-        'access_denied': constants.ACCESS_DENIED_ERROR,
-    }
 
     class Meta:
         model = Post
@@ -20,10 +15,14 @@ class PostSerializer(serializers.ModelSerializer):
     def get_likes(self, obj):
         return obj.post_likes.count()
 
-    def validate_change_own_post(self, user, post):
-        if user.id != post.author.id:
-            self.fail('access_denied')
 
-    def update(self, instance, validated_data):
-        self.validate_change_own_post(self.context["request"].user, instance)
-        return super().update(instance, validated_data)
+class PostSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("id", "title", "content", "created_at", "updated_at", "author", "likes", "image")
+        read_only_fields = ("id", "created_at", "updated_at", "author")
+
+    def get_likes(self, obj):
+        return obj.post_likes.count()
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,23 @@
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import viewsets, status, filters
 
 from blog.models import Post, Like
-from blog.serializers import PostSerializer
+from blog.serializers import PostSerializer, AuthorPostSerializer
 
 
-class PostView(viewsets.ModelViewSet):
+class AuthorPostView(viewsets.ModelViewSet):
     """
     retrieve:
-    Get a post
+    Get a author post
 
-    Get one post
+    Get one author post
 
     list:
-    Get list of posts
+    Get list of author posts
 
-    Get full list of posts
+    Get full list of author posts
 
     create:
     Create new author post
@@ -39,17 +39,36 @@ class PostView(viewsets.ModelViewSet):
 
     Partial update one author post
     """
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = AuthorPostSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
 
-    def destroy(self, request, *args, **kwargs):
-        post = self.get_object()
-        if request.user.id == post.author.id:
-            return super(PostView, self).destroy(self, request, *args, **kwargs)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self, *args, **kwargs):
+        return Post.objects.filter(author=self.request.user)
+
+
+class PostView(viewsets.ReadOnlyModelViewSet):
+    """
+    retrieve:
+    Get a post
+
+    Get one post
+
+    list:
+    Get list of posts
+
+    Get full list of posts
+
+    like:
+    Create or destroy like for post
+
+    Create or destroy one like for post
+    """
+    permission_classes = (AllowAny,)
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
     @action(detail=True, methods=['POST'], permission_classes=(IsAuthenticated,))
     def like(self, request, *args, **kwargs):
