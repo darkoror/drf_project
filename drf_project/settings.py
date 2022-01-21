5f57906f604dc5868073e7bd17ac74a2c541c01b
Roman Kravets <roman.kravets@vakoms.com>
2022-01-22 00:17:00 +0200

    remove token_blacklist

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'django_filters',
-    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt',
 
     'authentication',
     'blog',
