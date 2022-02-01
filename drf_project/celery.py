f98cc2f4edf4488ef811beab4bfaf75f575bae87
Roman Kravets <roman.kravets@vakoms.com>
2022-02-01 23:50:14 +0200

    set broker directly

--- drf_project/celery.py
+++ drf_project/celery.py
@@ -3,6 +3,7 @@ import os
 
 import celery.signals
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_project.settings')
 
@@ -12,7 +13,7 @@ def on_celery_setup_logging(**kwargs):
     pass
 
 
-app = Celery('drf_project')
+app = Celery('drf_project', broker=settings.BROKER_URL)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
